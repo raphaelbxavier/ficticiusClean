e5d285f8783bbe31b3fa127d409a4773a5cf4dce
Raphael Xavier <raphael.xavier@simplustec.com.br>
2020-08-01 19:40:46 -0300

    Repairing tests

--- src/test/java/br/com/totvs/api/veiculo/ficticiusClean/service/VeiculoServiceImplTest.java
+++ src/test/java/br/com/totvs/api/veiculo/ficticiusClean/service/VeiculoServiceImplTest.java
@@ -2,41 +2,32 @@ package br.com.totvs.api.veiculo.ficticiusClean.service;
 
 import br.com.totvs.api.veiculo.ficticiusClean.dto.ResponseDto;
 import br.com.totvs.api.veiculo.ficticiusClean.entity.Veiculo;
+import br.com.totvs.api.veiculo.ficticiusClean.mapper.VeiculoMapper;
 import br.com.totvs.api.veiculo.ficticiusClean.repository.VeiculoRepository;
 import org.junit.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.platform.runner.JUnitPlatform;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
-@RunWith(JUnitPlatform.class)
 public class VeiculoServiceImplTest {
 
-    @Mock
-    private VeiculoRepository mockVeiculoRepository;
-
-    @InjectMocks
-    private VeiculoService mockVeiculoService;
 
     @Test
     public void shouldSavedVeiculoComSucesso() {
         final Veiculo veiculo = new Veiculo(0L, "Fit", "Honda", "2015", LocalDate.of(2020, 7, 30), 12.0, 16.0);
 
-        given(mockVeiculoRepository.findById(veiculo.getId())).willReturn(Optional.empty());
-        given(mockVeiculoRepository.save(veiculo)).willAnswer(invocation -> invocation.getArgument(0));
+        VeiculoRepository mockVeiculoRepository = Mockito.mock(VeiculoRepository.class);
+        VeiculoService mockVeiculoService = new VeiculoServiceImpl(mockVeiculoRepository, null, new VeiculoMapper());
 
+        Mockito.when(mockVeiculoRepository.save(Mockito.any(Veiculo.class))).thenReturn(veiculo);
 
         final ResponseDto veiculoSalvo = mockVeiculoService.salvar(veiculo);
 
@@ -48,9 +39,11 @@ public class VeiculoServiceImplTest {
     @Test
     public void testCalcularGastos() {
 
+        VeiculoRepository mockVeiculoRepository = Mockito.mock(VeiculoRepository.class);
+        VeiculoService mockVeiculoService = new VeiculoServiceImpl(mockVeiculoRepository, null, new VeiculoMapper());
+
         final List<ResponseDto> result = mockVeiculoService.calcularGastos(3.81, 30.0, 120.0);
 
         assertThat(result).isNotNull();
-
     }
 }
